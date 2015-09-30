f82a7e10b1d64f02165100e7a41ee1e1fbb6ff29
Nukr <nukrs.w@gmail.com>
2015-10-01 00:37:24 +0800

    change express to koa

--- devServer.js
+++ devServer.js
@@ -1,27 +1,20 @@
-var path = require('path');
-var express = require('express');
-var webpack = require('webpack');
-var config = require('./webpack.config.dev');
+var path = require('path')
+var koa = require('koa')
+var webpack = require('webpack')
+var config = require('./webpack.config.dev')
+var serve = require('koa-static')
 
-var app = express();
-var compiler = webpack(config);
+var app = koa()
+var compiler = webpack(config)
 
-app.use(require('webpack-dev-middleware')(compiler, {
+app.use(require('koa-webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
-}));
+}))
 
-app.use(require('webpack-hot-middleware')(compiler));
+app.use(require('koa-webpack-hot-middleware')(compiler))
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.use(serve(__dirname))
 
-app.listen(3000, 'localhost', function(err) {
-  if (err) {
-    console.log(err);
-    return;
-  }
-
-  console.log('Listening at http://localhost:3000');
-});
+app.listen(3000)
+console.log('Listening at http://localhost:3000')
