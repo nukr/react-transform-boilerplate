49a487ff47cb1afde4078f3081c509d7e4ed69cc
Nukr <nukrs.w@gmail.com>
2015-12-06 23:37:56 +0800

    make standard compliant

--- src/App.js
+++ src/App.js
@@ -1,39 +1,44 @@
-import React, { Component } from 'react';
-import { NICE, SUPER_NICE } from './colors';
+import React, { Component } from 'react'
+import { NICE, SUPER_NICE } from './colors'
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { counter: 0 };
-    this.interval = setInterval(() => this.tick(), 1000);
+  constructor (props) {
+    super(props)
+    this.state = { counter: 0 }
+    this.interval = setInterval(() => this.tick(), 1000)
   }
 
-  tick() {
+  tick () {
     this.setState({
       counter: this.state.counter + this.props.increment
-    });
+    })
   }
 
-  componentWillUnmount() {
-    clearInterval(this.interval);
+  componentWillUnmount () {
+    clearInterval(this.interval)
   }
 
-  render() {
+  render () {
     return (
       <h1 style={{ color: this.props.color }}>
         Counter ({this.props.increment}): {this.state.counter}
       </h1>
-    );
+    )
   }
 }
 
+Counter.propTypes = {
+  increment: React.PropTypes.number,
+  color: React.PropTypse.string
+}
+
 export class App extends Component {
-  render() {
+  render () {
     return (
       <div>
         <Counter increment={100} color={NICE} />
         <Counter increment={5} color={SUPER_NICE} />
       </div>
-    );
+    )
   }
 }
